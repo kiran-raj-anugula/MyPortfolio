5ae5f09ee585ff8f3316aaa714aeae51d5279a6a
Kiran Raj <kiranraj.anugula+github@gmail.com>
2016-12-06 12:28:10 +0530

    Implemented Navigation

--- gulpfile.js
+++ gulpfile.js
@@ -70,7 +70,7 @@ gulp.task('jade', function(){
  * Watch html/md files, run jekyll & reload BrowserSync
  */
 gulp.task('watch', function () {
-    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch('assets/css/**/*', ['sass']);
     gulp.watch(['*.html', '_layouts/*.html', '_includes/*'], ['jekyll-rebuild']);
     gulp.watch(['_jadefiles/*.jade'], ['jade']);
 });
@@ -79,4 +79,4 @@ gulp.task('watch', function () {
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['browser-sync', 'watch' , 'jade']);
